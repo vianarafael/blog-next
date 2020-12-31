47ecd4feb9acea6903f2808a044f6cadd6379225
vianarafael <vianarafaelds@gmail.com>
2020-12-31 19:23:22 +0900

    first blog post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,10 +13,10 @@ export default function Index() {
       <div className="container">
         <div>
           <h1>
-            Hi, We're Next.js & Netlify<span className="fancy">.</span>
+            Hi, I'm Rafael<span className="fancy">.</span>
           </h1>
-          <span className="handle">@nextjs-netlify-blog</span>
-          <h2>A blog template with Next.js and Netlify.</h2>
+          <span className="handle"><a href="https://twitter.com/vianarafaelds" target="_blank">@vianarafaelds</a></span>
+          <h2>I write code and change diapers.</h2>
           <SocialList />
         </div>
       </div>
